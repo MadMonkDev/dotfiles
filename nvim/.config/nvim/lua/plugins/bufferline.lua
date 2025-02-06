9436b7018689822636c4f1cf2246209c87021483
MadMonkDev <aviralkmdubey@gmail.com>
2025-02-07 03:15:52 +0530

    modified bufferline.nvim

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,19 +1,26 @@
-return{
-  'akinsho/bufferline.nvim',
-  version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    require("bufferline").setup{
-      options = {
-        mode = 'buffer',
-        offsets = {
-          filetype = 'NvimTree',
-          text = "File Explorer",
-          highlight = "Directory",
-          seperator = true,
-        }
-      }
-    }
-  end;
-  vim.keymap.set('n', '<leader>bl', ':BufferLinePick<CR>')
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		vim.opt.termguicolors = true
+		require("bufferline").setup({
+			options = {
+				mode = "tabs",
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Tree",
+						separator = true,
+						text_align = "left",
+					},
+				},
+				diagnostics = "nvim_lsp",
+				separator_style = { "", "" },
+				modified_icon = "●",
+				show_close_icon = false,
+				show_buffer_close_icons = true,
+			},
+		})
+	end,
 }
